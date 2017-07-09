19e02cede36ade81ea69473a522114898b49837e
deltager <github@software.unf.dk>
2017-07-09 18:17:25 +0200

    Changes to TimeSettings.

--- app/src/main/java/com/example/deltager/simonsays5/TimeSettings.java
+++ app/src/main/java/com/example/deltager/simonsays5/TimeSettings.java
@@ -1,9 +1,12 @@
 package com.example.deltager.simonsays5;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.ToggleButton;
 
 /**
  * Created by deltager on 09-07-17.
@@ -12,13 +15,19 @@ import android.widget.NumberPicker;
 
 public class TimeSettings extends Activity {
 
-    NumberPicker np1;
-    NumberPicker np2;
+    private NumberPicker np1;
+    private NumberPicker np2;
+
+    private ToggleButton p1Toggle;
+    private ToggleButton p2Toggle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_settings);
+
+        p1Toggle = (ToggleButton) findViewById(R.id.ready1Btn);
+        p2Toggle = (ToggleButton) findViewById(R.id.ready2Btn);
 
         np1 = (NumberPicker) findViewById(R.id.numberPicker1);
         np1.setMaxValue(999);
@@ -31,8 +40,36 @@ public class TimeSettings extends Activity {
         np2.setMinValue(1);
         np2.setValue(60);
         np2.setWrapSelectorWheel(false);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        restartActivity();
+    }
 
 
+    public void ready1OnClick(View view) {
 
+        checkStartGame();
+    }
+
+    public void ready2OnClick(View view) {
+        checkStartGame();
+    }
+
+    private void checkStartGame() {
+        if (p1Toggle.isChecked() && p2Toggle.isChecked()) {
+            Intent intent = new Intent(this, TwoPlayerActivity.class);
+            intent.putExtra("player1Timer", np1.getValue());
+            intent.putExtra("player2Timer", np2.getValue());
+            startActivity(intent);
+        }
+    }
+
+    private void restartActivity() {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
     }
 }
